b92b888df63a84d41c61655787aa2750b3d9494e
soundmusic <wisu69@naver.com>
2019-04-30 22:38:45 +0900

    UI 어느정도 완성

---
+++ app/src/main/java/com/example/dreamislotto/StartActivity.java
@@ -0,0 +1,32 @@
+package com.example.dreamislotto;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_start);
+
+      Button button = (Button) findViewById(R.id.startBtn);
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent intent = new Intent(getApplicationContext(),preciousActivity.class);
+              startActivity(intent);
+          }
+      });
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dreamislotto/preciousActivity.java
@@ -0,0 +1,13 @@
+package com.example.dreamislotto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class preciousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_precious);
+    }
+}
